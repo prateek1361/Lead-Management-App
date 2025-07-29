1907e413c7b127c5ebf80f086445a1c86b04d3c5
prateek1361 <88934705+prateek1361@users.noreply.github.com>
2025-07-29 18:32:19 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -23,54 +23,52 @@ export default function Dashboard() {
       : leads?.slice(0, 3);
 
   return (
-    <div className="bg-body-secondary ">
-      <div className="row">
+    <div className="container-fluid bg-body-secondary">
+      <div className="row flex-column flex-md-row min-vh-100">
         <div className="col-md-3 bg-light  p-4 px-5">
-          <div className="container vh-100">
-            <h2 className=" mb-4">Anvaya CRM</h2>
-            <hr />
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Dashboard
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/leads">
-                  Leads
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/agents">
-                  Agents
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/reports">
-                  Reports
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/leadStatus">
-                  Lead Status
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/salesAgentView">
-                  Sales Agent View
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/setting">
-                  Settings
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <h2 className=" mb-4">Anvaya CRM</h2>
+          <hr />
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/leads">
+                Leads
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/agents">
+                Agents
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/reports">
+                Reports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/leadStatus">
+                Lead Status
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/salesAgentView">
+                Sales Agent View
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/setting">
+                Settings
+              </Link>
+            </li>
+          </ul>
         </div>
 
         <div className="col-md-9 p-4  px-5 ">
-          <div className="container mt-2">
+          <div className="container">
             <h1 className=" mb-5">Anvaya CRM Dashboard</h1>
             <hr />
 
@@ -79,7 +77,7 @@ export default function Dashboard() {
               {loading ? (
                 <p>Loading leads...</p>
               ) : (
-                <div className="d-flex gap-5">
+                <div className="d-flex flex-wrap gap-3">
                   {filteredLeads.slice(0, 3).map((lead) => (
                     <Link
                       key={lead._id}
@@ -88,7 +86,7 @@ export default function Dashboard() {
                     >
                       <div
                         key={lead._id}
-                        className="bg-light p-3 rounded shadow"
+                        className="bg-light p-3 rounded shadow w-100 w-sm-75 w-md-50"
                       >
                         <p className="fw-bold">{lead.name}</p>
                         <p className="text-muted">{lead.status}</p>
@@ -101,23 +99,23 @@ export default function Dashboard() {
 
             <div className="mb-6">
               <h2 className=" mb-4 mt-5">Lead Status</h2>
-              <div className="d-flex gap-5">
-                <div className="bg-light p-3 rounded shadow">
+              <div className="d-flex flex-wrap gap-3">
+                <div className="bg-light p-3 rounded shadow  w-100 ">
                   <p className="fw-medium">New</p>
                   <p>{newCount} Leads</p>
                 </div>
-                <div className="bg-light p-3 rounded shadow">
+                <div className="bg-light p-3 rounded shadow  w-100 ">
                   <p className="fw-medium">Contacted</p>
                   <p>{contactedCount} Leads</p>
                 </div>
-                <div className="bg-light p-3 rounded shadow">
+                <div className="bg-light p-3 rounded shadow  w-100">
                   <p className="fw-medium">Qualified</p>
                   <p>{qualifiedCount} Leads</p>
                 </div>
               </div>
             </div>
 
-            <div className="d-flex gap-3 mt-5">
+            <div className="d-flex flex-wrap gap-2 mt-5">
               <button
                 className="btn btn-outline-dark"
                 onClick={() => setStatusFilter("New")}
